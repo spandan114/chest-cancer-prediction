6815900f5880f3af952f9e940394222b17faa181
Spandan Joshi <spandanj685@gmail.com>
2024-12-12 22:33:31 +0530

    prepare base model vgg16

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -6,3 +6,15 @@ from pathlib import Path
 class DataIngestionConfig:
     root_dir: Path
     local_data_file: Path
+    
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
